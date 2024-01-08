55589258ac08bc9873acf18274e97a486db90c39
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2024-01-08 15:08:25 +0530

    new

---
+++ rentalapps/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import  *
+# Register your models here.
+
+admin.site.register(customusers)
+admin.site.register(Booking)
+admin.site.register(Car)
+admin.site.register(Company)
